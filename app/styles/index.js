0ec1ffb1950f32097523d5af28482d5325a67f27
Romulo Rosa Furtado <romuloros@gmail.com>
2016-04-18 16:39:39 -0300

    First functional app version

---
+++ app/styles/index.js
@@ -0,0 +1,15 @@
+var styles = {
+	transparentBg: {
+		background: 'transparent'
+	},
+	space: {
+    	marginTop: '15px',
+  	},
+  	centerText: {
+  		position: 'absolute',
+  		top: '50%',
+  		left: '45%'
+  	},
+}
+
+module.exports = styles;
